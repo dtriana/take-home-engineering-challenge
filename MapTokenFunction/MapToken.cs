9177d9ebcbb7c8c85ea7786034171915f8ada7ee
David Triana <david@davidtriana.com>
2021-02-15 15:22:30 -0500

    Update MapToken.cs

--- MapTokenFunction/MapToken.cs
+++ MapTokenFunction/MapToken.cs
@@ -1,10 +1,10 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Services.AppAuthentication;
+using System;
 
 namespace CseHomeWork.Maps
 {
